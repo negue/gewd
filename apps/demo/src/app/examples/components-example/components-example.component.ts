158d1d011a493724664b54b2e5f1505cfde54100
owehmer <45573843+owehmer@users.noreply.github.com>
2021-03-19 21:40:53 +0100

    Add control to test

--- apps/demo/src/app/examples/components-example/components-example.component.ts
+++ apps/demo/src/app/examples/components-example/components-example.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+const img1 = 'https://cdn.pixabay.com/photo/2013/07/12/12/58/tv-test-pattern-146649_1280.png'
+const img2 = 'https://images.ctfassets.net/hrltx12pl8hq/4plHDVeTkWuFMihxQnzBSb/aea2f06d675c3d710d095306e377382f/shutterstock_554314555_copy.jpg'
+
 @Component({
   selector: 'gewd-components-example',
   templateUrl: './components-example.component.html',
   styleUrls: ['./components-example.component.css']
 })
 export class ComponentsExampleComponent implements OnInit {
+  imgSource = img1;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  toggleImgSource() {
+    this.imgSource = this.imgSource === img1 ? img2 : img1;
+  }
 }
