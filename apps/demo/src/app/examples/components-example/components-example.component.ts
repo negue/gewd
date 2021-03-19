3b6856058948192312fe7203f1e3fc38ce561b42
owehmer <45573843+owehmer@users.noreply.github.com>
2021-03-19 22:10:16 +0100

    Added a new directive to trigger change detection for images inside the auto scale component when their source change sources

--- apps/demo/src/app/examples/components-example/components-example.component.ts
+++ apps/demo/src/app/examples/components-example/components-example.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
-const img1 = 'https://cdn.pixabay.com/photo/2013/07/12/12/58/tv-test-pattern-146649_1280.png'
-const img2 = 'https://images.ctfassets.net/hrltx12pl8hq/4plHDVeTkWuFMihxQnzBSb/aea2f06d675c3d710d095306e377382f/shutterstock_554314555_copy.jpg'
+const img1 = '/assets/example_md.png';
+const img2 = '/assets/example-bg.png';
 
 @Component({
   selector: 'gewd-components-example',
