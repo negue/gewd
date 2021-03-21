307497869fc91f081b747ce3916038236acde7e7
negue <eugen.bolz@gmail.com>
2021-03-21 23:44:19 +0100

    fix mat-utils and readme

--- libs/mat-utils/material-icons/register_icons.module.ts
+++ libs/mat-utils/material-icons/register_icons.module.ts
@@ -1,15 +1,10 @@
-import {
-  Inject,
-  InjectionToken,
-  ModuleWithProviders,
-  NgModule,
-} from '@angular/core';
+import { Inject, InjectionToken, ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {registerIcons, RegistryIconsPair} from "./register_icons";
-import {MatIconRegistry} from "@angular/material/icon";
-import {DomSanitizer} from "@angular/platform-browser";
+import { registerIcons, RegistryIconsPair } from './register_icons';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
-export const REGISTORY_ICONS_PAIR_TOKEN = new InjectionToken<RegistryIconsPair>('@gewd/mat-utils/BootstrapComponentConfig');
+export const REGISTORY_ICONS_PAIR_TOKEN = new InjectionToken<RegistryIconsPair>('@gewd/mat-utils/RegistryIconsPair');
 
 @NgModule({
   declarations: [],
