b41a80f01f327ecbac237b8889310386a822c224
negue <negue@users.noreply.github.com>
2024-04-17 03:09:58 +0000

    deploy: aaa04bf2fbb0513f3c116635bb66a8024592aaf8

---
+++ pr_135/assets/prism/prism-plugin-bracket-match.worker-func.js
@@ -0,0 +1,63 @@
+(function () {
+
+	if (typeof self === 'undefined' || typeof Prism === 'undefined') {
+		return;
+	}
+
+	const PAIRS = [
+	  [
+	    '[', ']'
+    ],
+    [
+      '{', '}'
+    ],
+    [
+      '(', ')'
+    ],
+    /*[
+      '<', '>'
+    ]*/
+  ]
+
+	const bracketsToWorkWith = [];
+
+	for (const p of PAIRS) {
+	  bracketsToWorkWith.push(...p);
+  	}
+
+  const currentDepth = {
+    '[': 0,
+    '{': 0,
+    '(': 0,
+    // '<': 0
+  }
+
+	Prism.hooks.add('wrap', (env) => {
+		if (env.type === 'punctuation' && bracketsToWorkWith.includes(env.content)) {
+			let depthOfType = 0, depthClass = '';
+			for (const p of PAIRS) {
+				const startTag = p[0];
+				const endTag = p[1];
+
+				if (env.content === startTag) {
+					depthOfType = ++currentDepth[startTag];
+
+					depthClass = `depth-${depthOfType}`;
+					env.classes.push(depthClass);
+					break;
+				}
+
+				if (env.content === endTag) {
+					depthOfType = currentDepth[startTag];
+
+					depthClass = `depth-${depthOfType}`;
+
+					env.classes.push(depthClass);
+
+					currentDepth[startTag]--;
+					break;
+				}
+			}	
+		}
+	});
+}());
